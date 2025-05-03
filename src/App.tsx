365f663c7908418778207b4a6336b298748c332b
Rafael <rakenvi@gmail.com>
2025-05-03 11:42:49 -0300

    coomit aula 22

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
