47c54f31b15c687670453d72134310bbff9d71f2
Rafael <rakenvi@gmail.com>
2025-05-09 17:52:21 -0300

    commit aula 25

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,28 @@ function App() {
       <Container>
         <CountDown />
       </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>Task</label>
+            <input id='input' type='text'></input>
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
